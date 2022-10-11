bc1b5eb5cb1ce0e7d7a8dbcba479eb0b0c27d2bf
Don Sabu <108197083+don-sabu@users.noreply.github.com>
2022-10-11 20:33:48 +0530

    Update sparse_matrix.c

--- sparse_matrix.c
+++ sparse_matrix.c
@@ -7,9 +7,7 @@ void sparse_matrix(struct sparse sm[],int ro,int co,int ar[][co])
 {
 	int i,j,k=0;
 	for(i=0;i<ro;i++)
-	{
 		for(j=0;j<co;j++)
-		{
 			if(ar[i][j]!=0)
 			{
 				sm[k].row=i;
@@ -17,9 +15,6 @@ void sparse_matrix(struct sparse sm[],int ro,int co,int ar[][co])
 				sm[k].value=ar[i][j];
 				k++;
 			}
-		
-		}
-	}
 }
 int main()
 {
@@ -31,23 +26,25 @@ int main()
 	int a[r][c];
 	printf("Enter the elements in the matrix\n");
 	for(i=0;i<r;i++)
-	{
 		for(j=0;j<c;j++)
 		{
 			scanf("%d",&a[i][j]);
 			if(a[i][j]!=0)
-			{
 				n++;
-			}
 		
 		}
-	}
+    printf("The Matrix is:\n");
+    for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+			printf("%d  ",a[i][j]);
+        printf("\n");
+    }
 	struct sparse s[n];
 	sparse_matrix(s,r,c,a);
 	printf("SPARSE MATRIX REPRESENTATION\n");
 	printf("Row(%d)  Column(%d)  value(%d)\n",r,c,n);
 	for(i=0;i<n;i++)
-	{
 		printf("  %d        %d         %d\n",s[i].row,s[i].column,s[i].value);
-	}
+
 }
