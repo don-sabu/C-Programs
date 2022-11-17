2fa97c1bcb7493bdf2a1b44c5def12fe773d2ab6
Don Sabu <108197083+don-sabu@users.noreply.github.com>
2022-11-17 06:57:13 +0530

    Update post_eval.c

--- Data_Structures/post_eval.c
+++ Data_Structures/post_eval.c
@@ -1,58 +1,59 @@
 #include<stdio.h>
 #include<ctype.h>
-int top=-1,stack[20];
-void push(char c)
+int top=-1;
+char st[50],P[50];
+char chr,ch;
+void push(char ch)
 {
-	top++;
-	stack[top]=(int)c;
+    top++;
+    st[top]=ch;
 }
-int pop()
+char pop()
 {
-	return stack[top--];
-}
-int power(int x,int y)
-{
-	int res=1,i;
-	for(i=0;i<y;i++)
-		res*=x;
-	return res;
+    ch=st[top];
+    top--;
+    return(ch);
 }
 int main()
 {
-	char exp[20],c;
-	int i=0,a,b;
-	printf("Enter the postfix expression: ");
-	scanf("%s",exp);
-	while(exp[i]!='\0')
-	{
-		c=exp[i];
-		if(isdigit(c))
-			push(c-'0');
-		else
-		{
-			a=pop();
-			b=pop();
-			switch(c)
-			{
-				case '+':
-					push(a+b);
-					break;
-				case '-':
-					push(a-b);
-					break;
-				case '*':
-					push(a*b);
-					break;
-				case '/':
-					push(a/b);
-					break;
-				case '^':
-					int x = power(a,b);
-					push(x);
-					break;
-			}
-		}
-		i++;
-	}
-	printf("The result is : %d\n",stack[0]);
+    int i,val=0,a,b;
+    printf("Enter a postfix expression with closing bracket\n");
+    for(i=0;P[i-1]!=')';i++)
+    {
+        scanf("%c",&P[i]);
+    }
+    i=0;
+    do
+    {
+        chr=P[i];
+        i++;
+        if(isdigit(chr))
+            push(chr- '0');
+        else if(chr=='+'||chr=='-'||chr=='/'||chr=='*'||chr=='^')
+        {
+           a=pop();
+           b=pop();
+           printf("%d  %d\n",a,b);
+           switch(chr)
+           {
+                case '+':
+                        val=b+a;
+                        break;
+                case '-':
+                        val=b-a;
+                        break;
+                case '*':
+                        val=b*a;
+                        break;
+                case '/':
+                        val=b/a;
+                        break;
+                case '^':
+                        val=b^a;
+                        break;
+           }
+           push(val);
+        }
+    }while(chr!=')');
+    printf("Evaluated value: %d",st[top]);
 }
