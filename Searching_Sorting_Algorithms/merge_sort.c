bdce14a4641d168d9396ef1e6943a4f8053c596c
Don Sabu <108197083+don-sabu@users.noreply.github.com>
2022-12-13 15:19:08 +0530

    Create merge_sort.c

---
+++ Searching_Sorting_Algorithms/merge_sort.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+void merge(int arr[],int l,int m,int u)
+{
+  int i, j, k;
+	int n1 = m - l + 1;
+	int n2 = u - m;
+	int L[n1], R[n2];
+	for (i = 0; i < n1; i++)
+		L[i] = arr[l + i];
+	for (j = 0; j < n2; j++)
+		R[j] = arr[m + 1 + j];
+	i = 0;
+	j = 0; 
+	k = l; 
+	while (i < n1 && j < n2) {
+		if (L[i] <= R[j]) {
+			arr[k] = L[i];
+			i++;
+		}
+		else {
+			arr[k] = R[j];
+			j++;
+		}
+		k++;
+	}
+	while (i < n1) {
+		arr[k] = L[i];
+		i++;
+		k++;
+	}
+	while (j < n2) {
+		arr[k] = R[j];
+		j++;
+		k++;
+	}
+}
+void mergesort(int ar[],int lb,int ub)
+{
+    if(lb<ub)
+    {
+        int mid=lb+(ub-lb)/2;
+        mergesort(ar,lb,mid);
+        mergesort(ar,mid+1,ub);
+        merge(ar,lb,mid,ub);
+    }
+}
+int main()
+{
+    int n,i;
+    printf("Enter the number of elements: ");
+    scanf("%d",&n);
+    int a[n];
+    printf("Enter %d elements\n",n);
+    for(i=0;i<n;i++)
+        scanf("%d",&a[i]);
+    mergesort(a,0,n-1);
+    printf("Sorted array:");
+	for(i=0;i<n;i++)
+		printf("  %d",a[i]);
+	printf("\n");
+}
