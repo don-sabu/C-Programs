2b4e8b3dba8d9142f2a59f179ecb264c45964100
Don Sabu <108197083+don-sabu@users.noreply.github.com>
2022-12-22 12:37:21 +0530

    Update Hash.c

--- other_programs/Hash.c
+++ other_programs/Hash.c
@@ -1,94 +1,68 @@
 #include<stdio.h>
 #include<stdlib.h>
-int tb,n,table[10];
 struct node
 {
-	int data;
-	struct node* link;
-}*head[10]={0},*temp,*newnode;
-void insert(int key)
+    int value;
+    struct node *next;
+}*chain;
+void insert(struct node *head[],int n,int key)
 {
-	newnode=(struct node*)malloc(sizeof(struct node));
-	newnode->data=key;
-	newnode->link=0;
-	n=key%tb;
-	if(head[n]==0)
-		head[n]=newnode;
-	else
-	{
-		temp=head[n];
-		while(temp->link!=0)
-			temp=temp->link;
-		temp->link=newnode;
-	}
+    int hash = key%n;
+    struct node *newnode = (struct node*)malloc(sizeof(struct node));
+    newnode->value=key;
+    newnode->next=NULL;
+    if(head[hash]==NULL)
+        head[hash]=newnode;
+    else
+    {
+        chain=head[hash];
+        while(chain->next!=NULL)
+            chain=chain->next;
+        chain->next=newnode;
+    }
 }
-void search()
+void display(struct node *head[],int n)
 {
-	int key;
-	printf("Enter the key to be searched: ");
-	scanf("%d",&key);
-	n=key%tb;
-	if(head[n]==0)
-		printf("Element not found\n");
-	else
-	{
-		temp=head[n];
-		int i=1;
-		while(temp!=0)
-		{
-			if(temp->data==key)
-			{
-				printf("Element found at index %d in column %d\n",n,i);
-				break;
-			}
-			temp=temp->link;
-			i++;
-		}
-		if(temp==0)
-			printf("Element not found\n");
-	}
+    printf("==========================\n");
+    printf("--------Hash Table--------\n");
+    printf("--------------------------\n");
+    printf(" Index     Key\n");
+    printf("--------------------------\n");
+    for(int i=0;i<n;i++)
+    {
+        printf("   %d",i);
+        printf("       ");
+        chain=head[i];
+        if(chain==NULL)
+            printf("null");
+        else
+            while(chain!=NULL)
+            {
+                printf("%d",chain->value);
+                chain=chain->next;
+                if(chain!=NULL)
+                    printf("-->");
+            }
+        printf("\n");
+    }
+    printf("==========================\n");
 }
-void display()
-{	
-	printf("------The Hashed Table------\n");
-	for(int i=0;i<tb;i++)
-	{
-		printf("\t%d-->",i);
-		temp=head[i];
-		while(temp!=0)
-		{
-			printf("%d-->",temp->data);
-			temp=temp->link;
-		}
-		printf("\n");
-	}
-}
-
 int main()
-{	
-	int choice;
-	printf("Enter table size: ");
-	scanf("%d",&tb);
-	printf("Enter key values:\n");
-	for(int i=0;i<tb;i++)
-		scanf("%d",&table[i]);
-	for(int i=0;i<tb;i++)
-		insert(table[i]);
-	do
-	{
-		printf("Options\n1.Search\t2.Display\t3.Exit\n");
-		printf("Enter your choice: ");
-		scanf("%d",&choice);
-		switch(choice)
-		{
-			case 1: search();
-					break;
-			case 2: display();
-					break;
-			case 3: printf("Terminated....\n");
-					break;
-			default:printf("Invalid choice\n");
-					break;
-		}
-	}while(choice!=3);
+{
+    int n,c,k,i;
+    printf("Enter the size of hash table: ");
+    scanf("%d",&n);
+    struct node *head[n];
+    for(i=0;i<n;i++)
+        head[i]=NULL;
+    printf("Enter number of keys: ");
+    scanf("%d",&k);
+    int key[k];
+    printf("Enter %d key values\n",k);
+    for(i=0;i<k;i++)
+    {
+        scanf("%d",&key[i]);
+        insert(head,n,key[i]);
+    }
+    display(head,n);
 }
